e1a52feaa56a5ae4875712d7dad757e12caabf28
Christopher <christopher.bondier@etu.univ-lyon1.fr>
2023-11-21 16:02:08 +0100

    ajout images

---
+++ public/scriptProjet.js
@@ -0,0 +1,8 @@
+var nbr_image = 6;
+var asideElement = document.querySelector("aside");
+for (let index = 1; index < nbr_image; index++) {
+  let img = document.createElement("img");
+  img.src = "img/img" + index + ".jpg";
+  img.width = 75;
+  asideElement.appendChild(img);
+}
