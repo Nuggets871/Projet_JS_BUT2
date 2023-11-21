fa4e7bdc19eccaf8c7f3642864b73867a1d93c2f
Christopher <christopher.bondier@etu.univ-lyon1.fr>
2023-11-21 16:42:55 +0100

    ajout zone depot

--- public/scriptProjet.js
+++ public/scriptProjet.js
@@ -1,8 +1,25 @@
 var nbr_image = 6;
+var nbr_depot = 4;
 var asideElement = document.querySelector("aside");
-for (let index = 1; index <= nbr_image; index++) {
-  let img = document.createElement("img");
-  img.src = "img/img" + index + ".jpg";
-  img.width = 75;
-  asideElement.appendChild(img);
+var articleElement = document.querySelector("main");
+
+function ajout_image_aside() {
+  for (let index = 1; index <= nbr_image; index++) {
+    let img = document.createElement("img");
+    img.src = "img/img" + index + ".jpg";
+    img.width = 75;
+    asideElement.appendChild(img);
+  }
 }
+
+function ajout_depot() {
+  for (let index = 1; index <= nbr_depot; index++) {
+    let img = document.createElement("img");
+    img.classList.add("depot");
+    img.alt = "zone " + index;
+    articleElement.appendChild(img);
+  }
+}
+
+ajout_image_aside();
+ajout_depot();
