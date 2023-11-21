27afdc144f09b0eced76e97c37cf9f0f000ccf09
Christopher <christopher.bondier@etu.univ-lyon1.fr>
2023-11-21 17:51:45 +0100

    ajout listener sur la partie image + déplacement des images

--- public/scriptProjet.js
+++ public/scriptProjet.js
@@ -7,7 +7,7 @@ function ajout_image_aside() {
   for (let index = 1; index <= nbr_image; index++) {
     let img = document.createElement("img");
     img.src = "img/img" + index + ".jpg";
-    img.width = 75;
+    img.width = 50;
     asideElement.appendChild(img);
   }
 }
@@ -16,6 +16,7 @@ function ajout_depot() {
   for (let index = 1; index <= nbr_depot; index++) {
     let img = document.createElement("img");
     img.classList.add("depot");
+    img.id = "img" + index;
     img.alt = "zone " + index;
     mainElement.appendChild(img);
   }
@@ -26,11 +27,25 @@ mainElement.addEventListener("click", depot_selectionne);
 function depot_selectionne(evt) {
   console.log("test");
   console.log("classe : " + evt.target.className);
+  // on verifie si l'element est déjà selectionné
   if (evt.target.className == "depot selected") {
-    // on verifie si l'element est déjà selectionner
     evt.target.classList.remove("selected"); // on lui remet son style de base si c'est le cas
   } else {
     evt.target.classList.add("selected"); // sinon on lui ajoute la classe "selected" pour mettre les bordures en rouge
+  }
+}
+
+asideElement.addEventListener("click", image_selectionne);
+
+function image_selectionne(evt) {
+  console.log(evt.target.src);
+  for (let index = 1; index <= nbr_depot; index++) {
+    let img = document.getElementById("img" + index);
+    if (img.className == "depot selected") {
+      console.log("test3");
+      img.src = evt.target.src;
+      img.classList.remove("selected");
+    }
   }
 }
 
